e05b9644812be096685270baf141e58cae324885
Dihak <dihak@digitalkode.com>
2022-11-20 14:19:21 +0700

    Add input jari2 and tinggi

--- uts/Main.java
+++ uts/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Tabung {
 	double jariJari;
 
@@ -15,7 +17,15 @@ class Tabung {
 
 public class Main {
 	public static void main(String[] args) {
-		Tabung tabung = new Tabung(10, 20);
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Masukkan jari-jari: ");
+		double jariJari = scanner.nextDouble();
+
+		System.out.print("Masukkan tinggi: ");
+		double tinggi = scanner.nextDouble();
+
+		Tabung tabung = new Tabung(jariJari, tinggi);
 		System.out.println("Jari-jari: " + tabung.jariJari);
 		System.out.println("Tinggi: " + tabung.tinggi);
 		System.out.println("Volume: " + tabung.getVolume());
